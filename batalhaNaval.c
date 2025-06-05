58029bab19b8224c521f47528302e2db84cb9fed
Gabriel Messias <202505212233@alunos.estacio.br>
2025-06-05 14:49:11 +0000

    atualizando para desafio aventureiro completo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
+#define L 10
+#define C 10
+
 int main () {
 
     // 0 = agua
     // 3 = navio
 
-    int matriz[10][10] = {0};
+    int matriz[L][C] = {0};
 
     // navio 1 na linha 2 (horizontal)
 
-    matriz [2][2] = 3;
-    matriz [2][3] = 3;
-    matriz [2][4] = 3;
+    matriz [9][2] = 3;
+    matriz [9][3] = 3;
+    matriz [9][4] = 3;
 
     // navio 2 na coluna 7 (vertical)
 
@@ -20,6 +23,19 @@ int main () {
     matriz [7][7] = 3;
     matriz [8][7] = 3;
     matriz [9][7] = 3;
+
+    // navio 3 na diagonal 1 (diagonal principal) esquema tabuleiro[i][i]
+
+    matriz [1][1] = 3;    
+    matriz [2][2] = 3;    
+    matriz [3][3] = 3;
+    matriz [4][4] = 3;
+
+    // navio 4 na diagonal 2 (diagonal secundária) esquema tabuleiro[i][9-i]
+
+    matriz [1][9] = 3;    
+    matriz [2][8] = 3;    
+    matriz [3][7] = 3;
 
     printf ("Tabuleiro de Batalha Naval\n\n");
 
